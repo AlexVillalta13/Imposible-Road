0810b39a24ae6f643c71f9d2ad515858b867d6fd
Alex <>
2020-05-07 16:16:43 +0200

    - Added particles when scoring

--- Assets/Scripts/Gameplay/ScoreBox.cs
+++ Assets/Scripts/Gameplay/ScoreBox.cs
@@ -8,6 +8,15 @@ public class ScoreBox : MonoBehaviour
 
     ScoreManager scoreManager;
 
+    GameObject meshGameObject;
+    ParticleSystem collisionEffect;
+
+    private void Awake()
+    {
+        collisionEffect = GetComponentInChildren<ParticleSystem>();
+        meshGameObject = transform.GetChild(0).gameObject;
+    }
+
     public void Init(ScoreManager scoreManager)
     {
         this.scoreManager = scoreManager;
@@ -16,10 +25,13 @@ public class ScoreBox : MonoBehaviour
     public void UpdateHoldingScore(int score)
     {
         scoreHolding = score;
+        meshGameObject.SetActive(true);
     }
 
     public void SumScore()
     {
         scoreManager.AddScore(scoreHolding);
+        collisionEffect.Play();
+        meshGameObject.SetActive(false);
     }
 }
