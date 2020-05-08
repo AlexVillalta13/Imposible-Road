4425152d38ab8b24043bf97d834a8a6efc61dbfa
Alex <>
2020-05-08 19:27:55 +0200

    - Fixed button

--- Assets/Scripts/Gameplay/Player/PlayerController_FSM.cs
+++ Assets/Scripts/Gameplay/Player/PlayerController_FSM.cs
@@ -37,7 +37,7 @@ public class PlayerController_FSM : MonoBehaviour
     [SerializeField] LayerMask rampLayer = 8;
     public LayerMask RampLayer { get { return rampLayer; } }
 
-    PlayerInput playerInput;
+    PlayerTouchHandler playerInput;
 
     // STATES
     public PlayerBaseState CurrentState { get; private set; }
@@ -61,7 +61,7 @@ public class PlayerController_FSM : MonoBehaviour
         TouchSimulation.Enable();
 
         rigid = GetComponent<Rigidbody>();
-        playerInput = GetComponent<PlayerInput>();
+        playerInput = GetComponent<PlayerTouchHandler>();
     }
 
     private void Update()
