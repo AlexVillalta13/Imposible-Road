bd5af98807bea80df60b80e6cd6b9daa900af56f
Alex <>
2020-05-11 21:27:09 +0200

    - Main Menu UI setup

---
+++ Assets/Scripts/UI/ButtonPressed.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class ButtonPressed : MonoBehaviour
+{
+    [SerializeField] RectTransform giftImageRectTransform = null;
+
+    public void MoveDownImageWhenPressed()
+    {
+        giftImageRectTransform.sizeDelta = new Vector2(200, 180);
+    }
+
+    public void MoveUpImageWhenPressed()
+    {
+        giftImageRectTransform.sizeDelta = new Vector2(200, 200);
+    }
+}
