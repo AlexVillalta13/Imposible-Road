5a87cb354b5042d0b7db652db6db462c68eaa037
Alex <>
2020-05-12 12:07:52 +0200

    - Score screen setup

--- Assets/Scripts/UI/ButtonPressed.cs
+++ Assets/Scripts/UI/ButtonPressed.cs
@@ -7,14 +7,22 @@ using UnityEngine.UI;
 public class ButtonPressed : MonoBehaviour
 {
     [SerializeField] RectTransform giftImageRectTransform = null;
+    float height;
+    float tenPercentageToAdd;
+
+    private void Awake()
+    {
+        height = giftImageRectTransform.rect.height;
+        tenPercentageToAdd = height * 10f / 100f;
+    }
 
     public void MoveDownImageWhenPressed()
     {
-        giftImageRectTransform.sizeDelta = new Vector2(200, 180);
+        giftImageRectTransform.sizeDelta = new Vector2(200, height - tenPercentageToAdd);
     }
 
     public void MoveUpImageWhenPressed()
     {
-        giftImageRectTransform.sizeDelta = new Vector2(200, 200);
+        giftImageRectTransform.sizeDelta = new Vector2(200, height);
     }
 }
