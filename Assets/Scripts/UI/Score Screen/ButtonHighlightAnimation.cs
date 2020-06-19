53328e78fe0a1c29148562723a6098bf3edeb0fc
Alex <>
2020-06-19 17:25:07 +0200

    - Buttons yo see ads and to buy skin has animation to highlight them

---
+++ Assets/Scripts/UI/Score Screen/ButtonHighlightAnimation.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonHighlightAnimation : MonoBehaviour
+{
+    [SerializeField] float changeSpriteTime = 0.4f;
+    [SerializeField] Sprite orangeButtonSprite = null;
+
+    Image image;
+    Sprite baseSprite;
+
+    private void Awake()
+    {
+        image = GetComponent<Image>();
+        baseSprite = image.sprite;
+    }
+
+    private void OnEnable()
+    {
+        StartCoroutine(HighlightAnimation());
+    }
+
+    private IEnumerator HighlightAnimation()
+    {
+        WaitForSeconds timeToChangeSprite = new WaitForSeconds(changeSpriteTime);
+        while(true)
+        {
+            yield return timeToChangeSprite;
+            image.sprite = orangeButtonSprite;
+
+            yield return timeToChangeSprite;
+            image.sprite = baseSprite;
+        }
+    }
+}
