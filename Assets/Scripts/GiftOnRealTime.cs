ed0737833047c1f424409f5c8d8b24945929f7e6
Alex <>
2020-06-22 13:04:02 +0200

    - Daily gift functionality

---
+++ Assets/Scripts/GiftOnRealTime.cs
@@ -0,0 +1,65 @@
+﻿using Bayat.SaveSystem;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GiftOnRealTime : MonoBehaviour
+{
+    string timeSaved = "savedTime";
+    DateTime lastTimePlayed;
+    TimeSpan timeToShow;
+    [SerializeField] int hoursToGetGift = 12;
+    TimeSpan hoursToGiftTimeSpan;
+
+    public event Action<bool, TimeSpan> canPlayerGetGift;
+
+    private void Awake()
+    {
+        // TODO change to hours
+        hoursToGiftTimeSpan = new TimeSpan(0, 0, hoursToGetGift);
+    }
+
+    async void Start()
+    {
+        if (await SaveSystemAPI.ExistsAsync(timeSaved) == false)
+        {
+            StartTimer();
+        }
+        lastTimePlayed = await SaveSystemAPI.LoadAsync<DateTime>(timeSaved);
+    }
+
+    void Update()
+    {
+        Debug.Log("Current Time: " + System.DateTime.Now);
+
+        CalculateTimeDifference();
+        CanGetGift();
+    }
+
+    public void StartTimer()
+    {
+        lastTimePlayed = DateTime.Now;
+        SaveSystemAPI.SaveAsync(timeSaved, lastTimePlayed);
+    }
+
+    private void CalculateTimeDifference()
+    {
+        TimeSpan timeDifference = DateTime.Now - lastTimePlayed;
+        timeToShow = hoursToGiftTimeSpan - timeDifference;
+        Debug.Log("Time to show: " + timeToShow);
+
+    }
+
+    private void CanGetGift()
+    {
+        if(timeToShow > TimeSpan.Zero)
+        {
+            canPlayerGetGift?.Invoke(false, timeToShow);
+        }
+        else
+        {
+            canPlayerGetGift?.Invoke(true, TimeSpan.Zero);
+        }
+    }
+}
---
+++ Assets/Scripts/UI/Skins/DailyGiftDisplay.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DailyGiftDisplay : MonoBehaviour
+{
+    TextMeshProUGUI countdownText;
+    Button giftButton;
+
+    GiftOnRealTime giftOnRealTime;
+
+    private void Awake()
+    {
+        countdownText = GetComponentInChildren<TextMeshProUGUI>();
+        giftButton = GetComponentInChildren<Button>();
+
+        giftOnRealTime = FindObjectOfType<GiftOnRealTime>();
+    }
+
+    private void OnEnable()
+    {
+        giftOnRealTime.canPlayerGetGift += SetupButtonAndText;
+    }
+
+    private void OnDisable()
+    {
+        giftOnRealTime.canPlayerGetGift -= SetupButtonAndText;
+    }
+
+    private void SetupButtonAndText(bool canGetGift, TimeSpan countdown)
+    {
+        int hours = countdown.Hours;
+        int minutes = countdown.Minutes;
+        int seconds = countdown.Seconds;
+        countdownText.text = String.Format("{0:00}:{1:00}:{2:00}", hours, minutes, seconds);
+
+        if (canGetGift)
+        {
+            giftButton.interactable = true;
+        }
+        else
+        {
+            giftButton.interactable = false;
+        }
+    }
+}
