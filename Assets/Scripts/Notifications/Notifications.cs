aca162554cdf08fa79673442fdf32f5e33216a1d
Alex <>
2020-06-24 19:10:06 +0200

    - Notifications ready to test

---
+++ Assets/Scripts/Notifications/Notifications.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Notifications.Android;
+using UnityEngine;
+
+
+public class Notifications : MonoBehaviour
+{
+#if UNITY_ANDROID
+    AndroidNotificationChannel defaultNotificationChannel;
+    int notificationID;
+
+    void Start()
+    {
+        defaultNotificationChannel = new AndroidNotificationChannel()
+        {
+            Id = "default_Channel_ID",
+            Name = "default_Channel",
+            Description = "For All Notifications",
+            Importance = Importance.Default,
+        };
+
+        AndroidNotificationCenter.RegisterNotificationChannel(defaultNotificationChannel);
+    }
+
+    public void SendNotification(string title, string text, DateTime fireTime)
+    {
+        AndroidNotification notification = new AndroidNotification()
+        {
+            Title = title,
+            Text = text,
+            SmallIcon = "default",
+            LargeIcon = "default",
+            FireTime = fireTime,
+        };
+
+        notificationID = AndroidNotificationCenter.SendNotification(notification, defaultNotificationChannel.Id);
+    }
+#elif UNITY_IOS
+
+#endif
+}
---
+++ Assets/Scripts/Notifications/NotificationsManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NotificationsManager : MonoBehaviour
+{
+    [SerializeField] string title = "Free gift now";
+    [SerializeField] string text = "Open the game to get a free gift";
+    int hoursToNotification = 12;
+
+
+    Notifications notifications;
+
+    private void Awake()
+    {
+        notifications = GetComponent<Notifications>();
+    }
+
+    public void SendNotification()
+    {
+        if(notifications != null)
+        {
+            notifications.SendNotification(title, text, System.DateTime.Now.AddSeconds(hoursToNotification));
+        }
+    }
+}
